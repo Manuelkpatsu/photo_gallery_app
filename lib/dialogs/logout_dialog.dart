f95ad6395a8aae1a26e511a3ff3b585e3064167e
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2022-12-01 23:37:40 +0000

    Add dialogs

---
+++ lib/dialogs/logout_dialog.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart' show BuildContext;
+import 'package:photo_gallery_app/dialogs/generic_dialog.dart';
+
+Future<bool> showLogoutDialog(BuildContext context) {
+  return showGenericDialog<bool>(
+    context: context,
+    title: 'Log out',
+    content: 'Are you sure you want to log out?',
+    optionsBuilder: () => {
+      'Cancel': false,
+      'Log out': true,
+    },
+  ).then((value) => value ?? false);
+}
