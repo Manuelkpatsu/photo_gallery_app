7ac7bbc16e5a8649b52545e942f99814174283cd
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2022-12-02 00:33:54 +0000

    Add loading screen

---
+++ lib/loading/loading_screen.dart
@@ -0,0 +1,98 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:photo_gallery_app/loading/loading_screen_controller.dart';
+
+class LoadingScreen {
+  // singleton pattern
+  LoadingScreen._sharedInstance();
+  static final LoadingScreen _shared = LoadingScreen._sharedInstance();
+  factory LoadingScreen.instance() => _shared;
+
+  LoadingScreenController? _controller;
+
+  void show({required BuildContext context, required String text}) {
+    if (_controller?.update(text) ?? false) {
+      return;
+    } else {
+      _controller = _showOverlay(context: context, text: text);
+    }
+  }
+
+  void hide() {
+    _controller?.close();
+    _controller = null;
+  }
+
+  LoadingScreenController _showOverlay({
+    required BuildContext context,
+    required String text,
+  }) {
+    final streamText = StreamController<String>();
+    streamText.add(text);
+
+    // get the size
+    final state = Overlay.of(context);
+    final renderBox = context.findRenderObject() as RenderBox;
+    final size = renderBox.size;
+
+    final overlay = OverlayEntry(
+      builder: (context) {
+        return Material(
+          color: Colors.black.withAlpha(150),
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              constraints: BoxConstraints(
+                maxWidth: size.width * 0.8,
+                maxHeight: size.height * 0.8,
+                minWidth: size.width * 0.5,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 10),
+                    const CircularProgressIndicator(),
+                    const SizedBox(height: 10),
+                    StreamBuilder<String>(
+                      stream: streamText.stream,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Text(
+                            snapshot.data!,
+                            textAlign: TextAlign.center,
+                          );
+                        } else {
+                          return const SizedBox.shrink();
+                        }
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+    state?.insert(overlay);
+
+    return LoadingScreenController(
+      close: () {
+        streamText.close();
+        overlay.remove();
+        return true;
+      },
+      update: (text) {
+        streamText.add(text);
+        return true;
+      },
+    );
+  }
+}
